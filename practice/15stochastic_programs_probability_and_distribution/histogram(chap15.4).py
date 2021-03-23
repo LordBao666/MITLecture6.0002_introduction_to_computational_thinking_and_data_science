1453c017ba6f565424ef981ae59c6370dc8630d3
LordBao666 <916900021@qq.com>
2021-03-23 21:31:14 +0800

    Commit  part of code about Lecture 6

---
+++ practice/15stochastic_programs_probability_and_distribution/histogram(chap15.4).py
@@ -0,0 +1,17 @@
+"""
+@Author  : Lord_Bao
+@Date    : 2021/3/23
+
+"""
+import pylab
+import random
+
+vals = []
+
+for i in range(10001):
+    num_1 = random.choice(range(101))
+    num_2 = random.choice(range(101))
+    vals.append(num_1 + num_2)
+
+pylab.hist(vals, bins=10)
+pylab.show()
